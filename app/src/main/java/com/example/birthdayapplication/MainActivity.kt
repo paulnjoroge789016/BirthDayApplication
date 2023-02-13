a298cecd5a46c80588a114e55748c34d59f5b5ca
Paul Njoroge <pauln@letshego.com>
2023-02-13 18:35:29 +0300

    Made adjustments for students

--- app/src/main/java/com/example/birthdayapplication/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapplication/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,17 +38,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthDayGreetingsWithImage(name: String) {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(400.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    ) {
 
-        Image(painter = painterResource(id = R.drawable.birthday_image), contentDescription = "")
-        Text(
-            text = "Happy Birthday $name!",
-            fontSize = 30.sp
-        )
+//        Image(painter = painterResource(id = R.drawable.birthday_image), contentDescription = "")
 
-        TODO("Add another text that states who sent the birthday greetings")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(
+                    text = "Hello Paul",
+                    fontSize = 20.sp
+                )
+                Text(
+                    text = "Explore your favorite movies",
+                    fontSize = 15.sp
+                )
+
+            }
+            Image(modifier = Modifier.size(64.dp).clip(CircleShape), painter = painterResource(id = R.drawable.birthday_image), contentDescription = "")
+
+        }
 
     }
 }
